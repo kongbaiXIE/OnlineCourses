0482e0a95021ab486f81b5e11c452e40b20a820b
kongbaiXIE <2498529257@qq.com>
2023-03-15 09:42:06 +0800

    [A] 搭建项目基本环境

--- online-courses-content/online-courses-content-service/src/main/java/com/xzq/contentService/mapper/CourseTeacherMapper.java
+++ online-courses-content/online-courses-content-service/src/main/java/com/xzq/contentService/mapper/CourseTeacherMapper.java
@@ -1,7 +1,8 @@
-package com.xzq.content.service.mapper;
+package com.xzq.contentService.mapper;
 
-import generator.domain.CourseTeacher;
+import com.xzq.content.model.entity.CourseTeacher;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author 谢志强
@@ -9,6 +10,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @createDate 2023-03-14 23:12:12
 * @Entity generator.domain.CourseTeacher
 */
+@Mapper
 public interface CourseTeacherMapper extends BaseMapper<CourseTeacher> {
 
 }
