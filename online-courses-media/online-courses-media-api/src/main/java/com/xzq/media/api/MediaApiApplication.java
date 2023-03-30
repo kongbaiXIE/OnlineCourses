f11c5d894d657b286a1d0a7e843c251cd3741f05
kongbaiXIE <2498529257@qq.com>
2023-03-30 14:43:47 +0800

    [A] 搭建媒资管理模块基本框架

---
+++ online-courses-media/online-courses-media-api/src/main/java/com/xzq/media/api/MediaApiApplication.java
@@ -0,0 +1,11 @@
+package com.xzq.media.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediaApiApplication.class,args);
+    }
+}
